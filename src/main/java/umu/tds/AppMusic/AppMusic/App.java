e8f2635b577b8ce743f572fcde508cc112935b61
mmcre <mmcre@192.168.1.137>
2023-10-28 10:10:22 +0200

    pc de mesa

--- src/main/java/umu/tds/AppMusic/AppMusic/App.java
+++ src/main/java/umu/tds/AppMusic/AppMusic/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World! :))" );
         System.out.println("fin prueba");
-        
+        System.out.println("pc de mesa");
         
     }
 }
